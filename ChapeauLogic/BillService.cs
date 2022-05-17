04dd1b97bc571aacdac3a3533e17c5390acda365
Joshua Andrea <joshua.andrea@hotmail.com>
2022-05-17 10:47:40 +0200

    basic bill stuff

---
+++ ChapeauLogic/BillService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChapeauLogic
+{
+    public class BillService
+    {
+
+    }
+}
---
+++ ChapeauModel/Bill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChapeauModel
+{
+    public class Bill
+    {
+        private float totalPrice;
+        private float VAT;
+        private float tip;
+    }
+}
