6cd121074ef9e8e778d3f60f99807200f440e5e5
Joshua Andrea <joshua.andrea@hotmail.com>
2022-05-19 23:53:45 +0200

    BillDao WIP

--- ChapeauLogic/BillService.cs
+++ ChapeauLogic/BillService.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ChapeauDAL;
+using ChapeauModel;
 
 namespace ChapeauLogic
 {
     public class BillService
     {
-
+        private BillDao billDAO;
+        
+        public Bill GetBill()
+        {
+            return billDAO.GetBill();
+        }
     }
 }
--- ChapeauModel/Bill.cs
+++ ChapeauModel/Bill.cs
@@ -5,13 +5,14 @@ namespace ChapeauModel
 {
     public class Bill
     {
+        public int BillId { get; set; }
         public List<BillItem> BillItems { get; set; }
         public float BaseBillPrice { get; set; }
         public float AlcoholicVAT { get; set; }
         public float NonAlcoholicVAT { get; set; }
-        public float TotalBillPrice { get; set; }
+        public float TotalBillPrice { get { return BaseBillPrice + AlcoholicVAT + NonAlcoholicVAT; } }
         public float Tip { get; set; }
-        public float TotalAmountPaid { get; set; }
+        public float TotalAmountPaid { get { return TotalBillPrice + Tip; } }
         public string RestaurantComment { get; set; }
         public bool IsPaid { get; set; }
     }
