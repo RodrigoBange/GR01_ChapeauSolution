106fa76bec1bb25cb67a516718da2c662dbb5588
Joshua Andrea <joshua.andrea@hotmail.com>
2022-05-20 12:08:43 +0200

    BillDao errors fixed

--- ChapeauDAL/BillDao.cs
+++ ChapeauDAL/BillDao.cs
@@ -12,7 +12,11 @@ namespace ChapeauDAL
     {
         public List<BillItem> GetBillItems(int billID)
         {
-            return new List<BillItem>();
+            string query = "SELECT m.itemName, COUNT(*) AS [count], M.price, M.tax, M.priceBeforeTax FROM ORDER_ITEMS AS O JOIN [MENU_ITEM] AS M ON O.itemID = M.itemID GROUP BY O.itemID, M.itemName, M.price, M.tax, M.priceBeforeTax";
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter();
+
+            return ReadBillItemTables(ExecuteSelectQuery(query, sqlParameters));
         }
 
         private List<BillItem> ReadTables(DataTable dataTable)
@@ -42,7 +46,7 @@ namespace ChapeauDAL
                 throw new Exception("Something went wrong while reading bill data from the database.");
             }
 
-            // Return list of menu items 
+            // Return list of bill items
             return billItems;
         }
     }
