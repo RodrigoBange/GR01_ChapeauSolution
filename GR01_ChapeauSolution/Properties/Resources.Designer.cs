8ebec244634927dc71ea61e55ed620f48ef92259
RodrigoBange <rodrigo99@live.nl>
2022-06-02 22:02:05 +0200

    Removed icon

--- GR01_ChapeauSolution/Properties/Resources.Designer.cs
+++ GR01_ChapeauSolution/Properties/Resources.Designer.cs
@@ -163,16 +163,6 @@ namespace ChapeauUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Home_Icon_New {
-            get {
-                object obj = ResourceManager.GetObject("Home_Icon_New", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Home_Icon_New1 {
             get {
                 object obj = ResourceManager.GetObject("Home_Icon_New1", resourceCulture);
